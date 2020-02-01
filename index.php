ae7e0e086029db7760b7113383d21dcc82ca02d5
¨Wangach¨ <jwangach@gmail.com>
2020-02-01 09:22:49 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Budgetry Start Page</title>
+	<link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+	
+	<script src="js/.jquery_3.4.1.js" type="text/javascript"></script>
+	<script src="js/index.js" type="text/javascript"></script>
+</body>
+</html>
