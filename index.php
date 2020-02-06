4acfaa566995fa00a60e74f83b9fe583b1a807a2
¨Wangach¨ <jwangach@gmail.com>
2020-02-06 23:12:17 +0300

    changes

--- index.php
+++ index.php
@@ -4,11 +4,52 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Budgetry Start Page</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-grid.css">
 	<link rel="stylesheet" href="css/index.css">
 </head>
 <body>
 	
+	<nav class="navbar navbar-light bg-faded">
+		<a class="navbar-brand" href="#">{4:Navbar}</a>
+		<ul class="nav navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Home</a>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="form-top">
+		<h3>Budgetry</h3>
+		<p>Please Fill In All Fields</p>
+	</div>
+	<form action="#" method="POST" class="budget">
+		<div class="form-group">
+			<label for="Type">Type: </label>
+			<select name="direct" id="direction" class="form-control">
+				<option value=""></option>
+				<option value="earning" id="">Credit</option>
+				<option value="spending" id="">Debit</option>
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="amount"> Amount:</label>
+			input.form-control
+		</div>
+	</form>
+
 	<script src="js/.jquery_3.4.1.js" type="text/javascript"></script>
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+	<script src="js/all.js" type="text/javascript"></script>
 	<script src="js/index.js" type="text/javascript"></script>
 </body>
 </html>
