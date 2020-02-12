2388803daa95084734affbae6ee019e7bc839a2b
Wangaruro <jwangach@gmail.com>
2020-02-12 07:52:18 +0300

    we made it!

--- index.php
+++ index.php
@@ -31,37 +31,65 @@ include 'scripts/master_query.php';
 		    
 		  </div>
 	</nav>
+	<div class="row">
+		<div class="col-md-6 col-lg-6 col-xl-12 col-sm-12">
+			<div class="form-top">
+				<h3>Budgetry</h3>
+				<p>Please Fill In All Fields</p>
+			</div>
+			<form action="index.php" method="POST" class="budget">
+				<div class="form-group">
+					<label for="Type">Type: </label>
+					<select name="direct" id="direction" class="form-control">
+						<option value=""></option>
+						<option value="earning" id="cr">Credit</option>
+						<option value="spending" id="db">Debit</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<label for="desc"> Description:</label>
+					<input class="form-control" type="text" placeholder="Describe Expenditure..." name="maelezo">
+				</div>
+				<div class="form-group">
+					<label for="amount"> Amount:</label>
+					<input class="form-control" type="number" placeholder="Enter Amount..." name="kiwango">
+				</div>
 
-	<div class="form-top">
-		<h3>Budgetry</h3>
-		<p>Please Fill In All Fields</p>
-	</div>
-	<form action="index.php" method="POST" class="budget">
-		<div class="form-group">
-			<label for="Type">Type: </label>
-			<select name="direct" id="direction" class="form-control">
-				<option value=""></option>
-				<option value="earning" id="cr">Credit</option>
-				<option value="spending" id="db">Debit</option>
-			</select>
-		</div>
-		<div class="form-group">
-			<label for="desc"> Description:</label>
-			<input class="form-control" type="text" placeholder="Describe Expenditure..." name="maelezo">
-		</div>
-		<div class="form-group">
-			<label for="amount"> Amount:</label>
-			<input class="form-control" type="number" placeholder="Enter Amount..." name="kiwango">
-		</div>
-
-		<button type="submit" class="btn btn-primary" name="register">Submit</button>
-	</form>
+				<button type="submit" class="btn btn-primary" name="register">Submit</button>
+			</form>
+		</div><!--end of column-->
+		<div class="col-md-6 col-lg-6 col-xl-12 col-sm-12">
+			<div class="row">
+				<div class="col-md-12 col-lg-12 col-xl-12 col-sm-12" id="cat">
+					<div class="cashathand"></div><!--cash-->
+					<p>Cash At Hand</p>
+					<h6><?php getCashAtHand(); ?></h6>
+				</div><!--End Of Column/-->
+				<div class="col-md-12 col-lg-12 col-xl-12 col-sm-12" id="cmd">
+					<div class="cashmade"></div><!--cash-->
+					<p>Cash Made</p>
+					<h6><?php getCashMade(); ?></h6>
+				</div><!--End Of Column/-->
+				<div class="col-md-12 col-lg-12 col-xl-12 col-sm-12" id="csp">
+					<div class="cashspent"></div><!--cash-->
+					<p>Cash Spent</p>
+					<h6><?php getCashSpent(); ?></h6>
+				</div><!--End Of Column/-->
+				<div class="col-md-12 col-lg-12 col-xl-12 col-sm-12" id="pmd">
+					<div class="profitmade"></div><!--cash-->
+					<p>Profit Made</p>
+					<h6><?php getProfit(); ?></h6>
+				</div><!--End Of Column/-->
+			</div><!--End of row-->
+		</div><!--end of column-->
+	</div><!--End of row-->
 	
 	<footer>
 		<p>Made By John Kimani &copy; <script>let current = new Date; document.write(current.getFullYear());</script></p>
 	</footer>
 	<script src="js/.jquery_3.4.1.js" type="text/javascript"></script>
 	<script src="js/bootstrap.js" type="text/javascript"></script>
+	<script src="js/bootstrap.bundle.js" type="text/javascript"></script>
 	<script src="js/all.js" type="text/javascript"></script>
 	<script src="js/index.js" type="text/javascript"></script>
 </body>
--- scripts/master_query.php
+++ scripts/master_query.php
@@ -13,16 +13,135 @@ if (isset($_POST['register'])) {
 	//We Fill The Data In THe Database Depending on the type eof cash
 	switch ($aina) {
 		case 'earning':
+			//XSince in this case we are earning, the credit value should be +
+			$moneyIn = $cash;
+			$moneyOut = 0;
+			$totals = $moneyIn + $moneyOut;
+			//Generate A Random Value
+			$random = ["a", "b", "c", "d", "e", "F", "1", "2", "3", "5", "6"];
+			$holder = "";
+
+			for ($i=0; $i < 8; $i++) {
+				$val1 = rand(0, 9);
+				$genVal = $random[$val1];
+				$holder = $holder.$genVal;
+			}
 			//Using the values gotten from the query above
-			$earnQuery = "INSERT INTO cash(id, description, credit, debit, amount, cashid) VALUES()";
+			$earnQuery = "INSERT INTO cash(id, description, credit, debit, amount, cashid) VALUES('', '$descr', '$moneyIn', '$moneyOut', '$totals', '$holder')";
+			$confirmation = mysqli_query($dbcon, $earnQuery);
+			if ($confirmation) {
+				$output = "<script";
+				$output .= "alert('The Transaction Has Been Recorded Successfully');";
+				$output .= "</script>";
+
+				echo $output;
+			}else{
+				$output = "<script";
+				$output .= "alert('Sorry! There Has Been An Error!');";
+				$output .= "</script>";
+
+				echo $output;
+			}
 			break;
 			case 'spending':
-			
+				//XSince in this case we are earning, the credit value should be +
+				$moneyIn = 0;
+				$moneyOut = $cash;
+				$totals = $moneyIn +- $moneyOut;
+				//Generate A Random Value
+				$random = ["a", "b", "c", "d", "e", "F", "1", "2", "3", "5", "6"];
+				$holder = "";
+
+				for ($i=0; $i < 8; $i++) {
+					$val1 = rand(0, 9);
+					$genVal = $random[$val1];
+					$holder = $holder.$genVal;
+				}
+				//Using the values gotten from the query above
+				$earnQuery = "INSERT INTO cash(id, description, credit, debit, amount, cashid) VALUES('', '$descr', '$moneyIn', '$moneyOut', '$totals', '$holder')";
+				$confirmation = mysqli_query($dbcon, $earnQuery);
+				if ($confirmation) {
+					$output = "<script";
+					$output .= "alert('The Transaction Has Been Recorded Successfully');";
+					$output .= "</script>";
+
+					echo $output;
+				}else{
+					$output = "<script";
+					$output .= "alert('Sorry! There Has Been An Error!');";
+					$output .= "</script>";
+
+					echo $output;
+				}
 			break;
 		
 		default:
 			
 			break;
+	}
+}
+function getCashAtHand()
+{	include 'db.php';
+	$todaysDates = date('Y-m-d');
+	$getData = "SELECT * FROM cash WHERE description LIKE '%cash at hand%' AND trans_tym LIKE '$todaysDates%'";
+	$specify = mysqli_query($dbcon, $getData);
+	if (mysqli_num_rows($specify) > 0) {
+		while ($row = mysqli_fetch_assoc($specify)) {
+			#get the data from the rows
+			$value = $row['amount'];
+			echo "KES ".$value;
+		}
+	}else{
+		echo "No Record Found!";
+	}
+}
+
+//Get Cash Made
+function getCashMade()
+{	include 'db.php';
+	$todaysDates = date('Y-m-d');
+	$getData = "SELECT SUM(credit) AS earnings FROM `cash` WHERE trans_tym LIKE '$todaysDates%' AND description NOT LIKE '%cash at hand%'";
+	$specify = mysqli_query($dbcon, $getData);
+	if (mysqli_num_rows($specify) > 0) {
+		while ($row = mysqli_fetch_assoc($specify)) {
+			#get the data from the rows
+			$value = $row['earnings'];
+			echo "KES ".$value;
+		}
+	}else{
+		echo "No Record Found!";
+	}
+}
+//Get Cash Spent
+function getCashSpent()
+{	include 'db.php';
+	$todaysDates = date('Y-m-d');
+	$getData = "SELECT SUM(debit) AS spendings FROM `cash` WHERE trans_tym LIKE '$todaysDates%' AND description NOT LIKE '%cash at hand%'";
+	$specify = mysqli_query($dbcon, $getData);
+	if (mysqli_num_rows($specify) > 0) {
+		while ($row = mysqli_fetch_assoc($specify)) {
+			#get the data from the rows
+			$value = $row['spendings'];
+			echo "KES ".$value;
+		}
+	}else{
+		echo "No Record Found!";
+	}
+}
+//Get Profit
+function getProfit()
+{	include 'db.php';
+	$todaysDates = date('Y-m-d');
+	$getData = "SELECT SUM(amount) AS profit FROM `cash` WHERE trans_tym LIKE '$todaysDates%' AND description NOT LIKE '%cash at hand%'";
+	$specify = mysqli_query($dbcon, $getData);
+	if (mysqli_num_rows($specify) > 0) {
+		while ($row = mysqli_fetch_assoc($specify)) {
+			#get the data from the rows
+			$value = $row['profit'];
+			echo "KES ".$value;
+		}
+	}else{
+		echo "No Record Found!";
 	}
 }
  ?>
